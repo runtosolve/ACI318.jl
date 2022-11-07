11c33c9a7e5a07e945b5d1bc94c85e645f2e037f
Cris Moen <cris.moen@runtosolve.com>
2022-11-07 11:26:17 -0500

    get going

---
+++ src/ACI318.jl
@@ -0,0 +1,7 @@
+module ACI318
+
+export v14
+include("v14.jl")
+using .v14
+
+end # module ACI318
---
+++ src/v14.jl
@@ -0,0 +1,39 @@
+module v14
+
+
+
+function table_14_5_6_1(;A1, A2, f_c, relative_geometric_conditions, design_code)
+
+    if relative_geometric_conditions == "wider on all sides"
+
+        Bn = minimum([sqrt(A2/A1)*(0.85*f_c*A1), 2*(0.85*f_c*A1)]
+
+    elseif relative_geometric_conditions == "Other"
+
+        Bn = 0.85*f_c*A1
+
+    end
+
+    action = "Bearing"
+    ϕ = table_21_2_1(action)
+
+    ϕBn = ϕ * Bn
+
+    return Bn, ϕBn
+
+end
+
+function table_21_2_1(action)
+
+    if action == "Bearing"
+
+        ϕ = 0.65
+
+    end
+
+    return ϕ
+
+end
+
+
+end
